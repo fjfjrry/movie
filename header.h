db607b83cd5db6498a607424d03249a241d13351
nonaninona <starcraft0529@gmail.com>
2020-04-11 15:52:59 +0900

    make structure

---
+++ header.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <malloc.h>
+#define TITLE_SIZE 20
+#define DIRECTOR_SIZE 20
+#define GENRE_SIZE 10
+
+typedef struct _node {
+
+	char title[TITLE_SIZE];
+	char director[DIRECTOR_SIZE];
+	int time;
+	char genre[GENRE_SIZE];
+
+	struct _node* next;
+
+}Node;
+
+typedef struct _list {
+	
+	int count;
+	Node* head;
+
+}List;
+
+void show(List *p);
+void init(List *p);
+void insert(char* title, char* director, int time, char* genre, List* p);
+void delete(List* p);
+void edit(int order, char* title, char* director, int time, char* genre);
+//search
+//sort
